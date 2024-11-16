3f2cfe98b2295b89c99396e28b3ba897ffa7932e
黃麗文 <131679484+Wendy0110@users.noreply.github.com>
2024-11-16 17:38:03 +0800

    Add files via upload

---
+++ 行動裝置程式設計_小朋友下樓梯/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.CommunityToolkit;
+using Xamarin.Essentials;
+using Plugin.SimpleAudioPlayer;
+using System.Diagnostics;
+
+namespace game
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new GamePage());
+        }
+    }
+}
---
+++ 行動裝置程式設計_小朋友下樓梯/Nails.cs
@@ -0,0 +1,50 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using Xamarin.Forms;
+
+namespace game
+{
+    public class Nails
+    {
+        public float x, y;
+        public float dy;
+        SKBitmap image;
+
+        public Nails(float x, float y, float dy, ContentPage cont)
+        {
+            this.x = x;
+            this.y = 2048 + y;
+            this.dy = dy;
+            Assembly assembly1 = cont.GetType().GetTypeInfo().Assembly;
+            string resourceID = "game.Resources.nails.png";
+            using (Stream stream = assembly1.GetManifestResourceStream(resourceID))
+            using (SKManagedStream skStream = new SKManagedStream(stream))
+            {
+                SKBitmap bmp = SKBitmap.Decode(skStream);
+                var dstInfo = new SKImageInfo(280, 40);
+                image = bmp.Resize(dstInfo, SKFilterQuality.Medium);
+            }
+        }
+        public bool Move()
+        {
+            y += dy;
+            if (y < 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void Draw(SKCanvas canvas)
+        {
+            if (x > 0)
+            {
+                canvas.DrawBitmap(image, x, y);
+            }
+        }
+    }
+}
